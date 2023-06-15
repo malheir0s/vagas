b7cc3cf12f608d11d688f593274a965ad7cadbf0
malheiros <dmalheiros77@gmail.com>
2023-06-15 12:57:15 -0300

    model adjustments and test

--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,61 @@
-const data  =  [
+const data = [
     {
         id: 1,
         name: "João Oliveira",
-        job: "Desenvolvedor"
+        job: "Desenvolvedor",
+        read_count: 0
     }
 ]
 
-function getByName (name){
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            return (data[i]);
-        }
+function getByName(name) {
+    user = data.find(user => user.name === name);
+    if (user) {
+        user.read_count += 1;
     }
+    return user;
 };
 
-function getAll (){
-    return data;  
+function getAll() {
+    return data;
 };
 
-function create (name, job){
-    var newUser = {
+function create(name, job) {
+    last_user = data[data.length - 1];
+    new_id = last_user ? last_user.id + 1 : 1;
+    var new_user = {
         name: name,
         job: job,
+        id: new_id,
+        read_count: 0
     }
 
-    data.push(newUser)
-    return newUser;
+    data.push(new_user)
+    return new_user;
 }
 
-function deleteByName(name){
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+function deleteByName(name) {
+    const indice = data.findIndex(user => user.name === name);
+    if (indice !== -1) {
+        data.splice(indice, 1);
+        return true;
+    } else {
+        return false;
     }
 }
 
-function updateById(id, name, job){
-    const reg = data.find(d => id == id);
-    reg.name = name;
-    reg.job = job;
+function updateById(id, name, job) {
+    const reg = data.find(user => user.id === id);
+
+    if (reg) {
+        reg.name = name;
+        reg.job = job;
+    }
     return reg;
 }
 
-function readCountByUserName(name){
-    return "Usuário " +  name  + "  foi lido 0 vezes.";
+function readCountByUserName(name) {
+    user = data.find(user => user.name === name);
+    return user ? user.read_count : false;
 };
 
 module.exports = {
--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,44 +3,96 @@ const model = require('../../src/models/user')
 describe('testing User Model', () => {
     describe('getAll', () => {
         test('should return an array of all users', () => {
-
-        });
-    });
-
-    describe('getByName', () => {
-        test('searching for an existing user: should return a user by name', () => {
-
-        });
-
-        test('searching for an user that does not exist: should return undefined', () => {
-
-        });
-    });
-
-    describe('deleteByName', () => {
-        test('deleting an existing user: the user is should be removed from the array, and should return true', () => {
-
-        });
-        test('trying to delete an user that does not exist: array should remain the same, and should return false', () => {
-
+            expect(model.getAll()).toStrictEqual([
+                {
+                    id: 1,
+                    name: "João Oliveira",
+                    job: "Desenvolvedor",
+                    read_count: 0
+                }
+            ])
         });
     });
 
     describe('create', () => {
         test('should insert a new json in the array, and return the new created user', () => {
+            expect(model.create('New User', 'Developer')).toStrictEqual(
+                {
+                    id: 2,
+                    name: "New User",
+                    job: "Developer",
+                    read_count: 0
+                }
+            )
+        });
+    });
 
+    describe('getByName', () => {
+        test('searching for an existing user: should return a user by name', () => {
+            expect(model.getByName('João Oliveira')).toStrictEqual({
+                id: 1,
+                name: "João Oliveira",
+                job: "Desenvolvedor",
+                read_count: 1
+            })
+        });
+
+        test('searching for an user that does not exist: should return undefined', () => {
+            expect(model.getByName('i do not exist')).toBe(undefined)
+        });
+    });
+
+    describe('deleteByName', () => {
+        test('deleting an existing user: the user should be removed from the array, and should return true', () => {
+            expect(model.deleteByName('João Oliveira')).toBe(true)
+            expect(model.getAll()).toStrictEqual([{
+                id: 2,
+                name: "New User",
+                job: "Developer",
+                read_count: 0
+            }])
+        });
+        test('trying to delete an user that does not exist: array should remain the same, and should return false', () => {
+            expect(model.deleteByName('i do not exist')).toBe(false)
+            expect(model.getAll()).toStrictEqual([{
+                id: 2,
+                name: "New User",
+                job: "Developer",
+                read_count: 0
+            }])
         });
     });
 
     describe('updateById', () => {
-        test('should update a user by ID', () => {
-
+        test('should update an user by ID', () => {
+            expect(model.updateById(2, 'Updated Name', 'Senior Developer')).toStrictEqual({
+                id: 2,
+                name: "Updated Name",
+                job: "Senior Developer",
+                read_count: 0
+            })
+            expect(model.getAll()).toStrictEqual([{
+                id: 2,
+                name: "Updated Name",
+                job: "Senior Developer",
+                read_count: 0
+            }])
+        });
+        test('trying to update an user that doest not exist, should return undefined.', () => {
+            expect(model.updateById(0, 'Updated Name', 'Senior Developer')).toBe(undefined)
         });
     });
 
     describe('readCountByUserName', () => {
         test('should return the count of user reads by name', () => {
-
+            expect(model.readCountByUserName('Updated Name')).toBe(0)
         });
+        test('after reading the user, count should be incremented.', () => {
+            model.getByName('Updated Name');
+            expect(model.readCountByUserName('Updated Name')).toBe(1)
+        })
+        test('Trying to read an user that does not exist, should return false.', () => {
+            expect(model.readCountByUserName('i do not exist')).toBe(false)
+        })
     });
 });
