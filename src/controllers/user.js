4b5d65669271d107282798eda83034b27352f2dd
malheiros <dmalheiros77@gmail.com>
2023-06-15 23:43:54 -0300

    user controller adjustments + unit tests

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,14 @@ const userModel = require('../models/user');
 
 const getByName = ( req, res, next ) => {
     var name =  req.query.name;
-    return res.getByName(name);
+
+    user = userModel.getByName(name);
+
+    if(user){
+        res.send(user);
+    } else {
+        res.status(400).json({'error': 'User not found.'})
+    }
 };
 
 const getAll = ( req, res, next ) => {
@@ -10,34 +17,51 @@ const getAll = ( req, res, next ) => {
 };
 
 const create = ( req, res, next ) => {
-    var name =  req.body.name;
-    var job =  req.body.job;
-    
-    res.send(userModel.create(name, job));
+    var {name, job} =  req.body;
+    if (typeof name !== 'string' || typeof job !== 'string'){
+        res.status(400).json({'error': 'Fields \'name\' and \'job\' are required and should be a string.'})
+    } else {
+        res.status(201).json(userModel.create(name, job));
+    }
 };
 
 
-function deleteByName (req, res) {
-  
+function deleteByName (req, res, next) {
     var name =  req.query.name;
 
-    userModel.deleteByName(name)
-
-    res.send("success");
+    if(userModel.deleteByName(name)){
+        res.send('success');
+    }{
+        res.status(400).json({'error': 'User not found.'});
+    }
 
 };
 
-function updateById(req, res) {
-    const id =  req.query.id;
+function updateById(req, res, next) {
+    var id =  req.query.id;
+
+    if ((/^\d+$/.test(id))){
+        id = parseInt(id)
+    } else{
+        return res.status(400).json({'error': 'parameter \'id\' should be a number.'})
+    }
+
     const {name, job} = req.body
-    res.send(userModel.updateById(id, name, job));
 
+    updatedUser = userModel.updateById(id, name, job)
+
+    if(updatedUser){
+        res.send(updatedUser);
+    } else{
+        res.status(400).json({'error': 'User not found.'})
+    }
 };
 
-function readCountByUserName(req, res){
+function readCountByUserName(req, res, next){
     var name =  req.query.name;
-    res.send(userModel.readCountByUserName(name));
-
+    read_count = userModel.readCountByUserName(name);
+    
+    (typeof read_count === 'number') ? res.send(read_count) : res.status(400).json({'error': 'User not found.'})
 };
 
 module.exports = {
--- test/controllers/user.test.js
+++ test/controllers/user.test.js
@@ -1,39 +1,142 @@
 const controller = require('../../src/controllers/user')
+const model = require('../../src/models/user')
+
+
+
 
 describe('testing User Controller', () => {
     describe('getAll', () => {
         test('should return an array of all users', () => {
+            const req = {};
+            const res = {
+                send: jest.fn().mockReturnThis()
+            };
 
+            controller.getAll(req, res);
+
+            expect(res.send).toHaveBeenCalledWith([
+                {
+                    id: 1,
+                    name: "João Oliveira",
+                    job: "Desenvolvedor",
+                    read_count: 0
+                }
+            ]);
         });
     });
 
     describe('getByName', () => {
         test('searching for an existing user: should return a user by name', () => {
+            const req = { query: { name: 'John' } };
+            const res = {
+                send: jest.fn()
+            };
+
+            model.getByName = jest.fn().mockReturnValueOnce([
+                {
+                    id: 2,
+                    name: "John",
+                    job: "Developer",
+                    read_count: 0
+                }
+            ]);
+
+            controller.getByName(req, res);
+
+            expect(res.send).toHaveBeenCalledWith([
+                {
+                    id: 2,
+                    name: "John",
+                    job: "Developer",
+                    read_count: 0
+                }
+            ]);
 
         });
 
         test('searching for an user that does not exist: should return status 400, and an error message.', () => {
+            const req = { query: { name: 'John' } };
+            const res = {
+                json: jest.fn(),
+                status: jest.fn().mockReturnThis()
+            };
+
+            model.getByName = jest.fn().mockReturnValueOnce(undefined);
+
+            controller.getByName(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ 'error': 'User not found.' });
 
         });
     });
 
     describe('create', () => {
-        test('passing name and job strings, should create and return the user. An id should be created for the user.', () => {
+        test('passing name and job strings, should create and return the user.', () => {
+            const req = { body: { name: 'John', job: 'developer' } };
+            const res = {
+                json: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis()
+            };
 
+            controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({ name: 'John', job: 'developer', read_count: 0, id: 2 });
         });
         test('not passing name or job fields, should return status 400 and an error message.', () => {
+            const req = { body: { name: 'John' } };
+            const res = {
+                json: jest.fn(),
+                status: jest.fn().mockReturnThis()
+            };
+
+            controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: 'Fields \'name\' and \'job\' are required and should be a string.' });
 
         });
         test('if name or job field is not an string, it should return status 400 and an error message.', () => {
+            const req = { body: { name: 1, job: 'dev' } };
+            const res = {
+                json: jest.fn(),
+                status: jest.fn().mockReturnThis()
+            };
+
+            controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: 'Fields \'name\' and \'job\' are required and should be a string.' });
 
         });
     });
 
     describe('deleteByName', () => {
         test('deleting an existing user: should return a success message', () => {
+            const req = { query: { name: 'João Oliveira' } };
+            const res = {
+                send: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
 
+            controller.deleteByName(req, res);
+
+            expect(res.send).toHaveBeenCalledWith('success');
         });
         test('trying to delete an user that does not exist: should return status 400 and an error message.', () => {
+            const req = { query: { name: 'not an User' } };
+            const res = {
+                json: jest.fn(),
+                status: jest.fn().mockReturnThis(),
+            };
+
+            model.deleteByName = jest.fn().mockReturnValueOnce(false);
+            controller.deleteByName(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ error: 'User not found.' });
 
         });
         test('trying to delete another user as an user without permission: should return status 403 and an error message.', () => {
@@ -43,11 +146,73 @@ describe('testing User Controller', () => {
 
     describe('updateById', () => {
         test('updating an existing user: should return the updated user data', () => {
+            const req = {
+                query: { id: '2' },
+                body: {
+                    name: 'New name',
+                    job: 'new job'
+                }
+            };
+            const res = {
+                send: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis()
+            };
+
+            controller.updateById(req, res);
+
+            expect(res.send).toHaveBeenCalledWith({
+                name: 'New name',
+                job: 'new job',
+                id: 2,
+                read_count: 0
+            });
 
         });
         test('trying to update an user that does not exist: should return status 400 and an error message.', () => {
+            const req = {
+                query: { id: '55' },
+                body: {
+                    name: 'New name',
+                    job: 'new job'
+                }
+            };
+            const res = {
+                json: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+            };
+
+            controller.updateById(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({
+                error: 'User not found.'
+            });
 
         });
+
+        test('should not be possible to accept an id that is not a number.', () => {
+            const req = {
+                query: { id: 'ab' },
+                body: {
+                    name: 'New name',
+                    job: 'new job'
+                }
+            };
+            const res = {
+                json: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+            };
+
+            controller.updateById(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({
+                error: 'parameter \'id\' should be a number.'
+            });
+
+        });
+
         test('trying to update another user as an user without permission: should return status 403 and an error message.', () => {
 
         });
@@ -55,9 +220,32 @@ describe('testing User Controller', () => {
 
     describe('readCountByUserName', () => {
         test('should return the count of user reads by name', () => {
+            const req = {
+                query: { name: 'New name' },
+            };
+            const res = {
+                send: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            };
 
+            controller.readCountByUserName(req, res);
+
+            expect(res.send).toHaveBeenCalledWith(0);
         });
         test('user does not exist: should return status 400 and an error message.', () => {
+            const req = {
+                query: { name: 'I do not exist' },
+            };
+            const res = {
+                json: jest.fn().mockReturnThis(),
+                status: jest.fn().mockReturnThis(),
+            };
+
+            controller.readCountByUserName(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({error: 'User not found.'});
 
         });
     });
